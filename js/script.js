b47b12aa4379100989f9049ecfc672310944be81
Yuri <yvhown@gmail.com>
2022-05-07 18:25:06 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let number = 5;
+const letBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
---
+++ js/script1.js
@@ -0,0 +1,15 @@
+
+
+console.log('arr'+ "- object");
+console.log(4 + "- object");
+
+let incr = 10,
+    decr = 10; 
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === "8");
